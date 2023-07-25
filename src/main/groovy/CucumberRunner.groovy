02c712d47266999d22e6de659735a19fbbbdad69
dergil <cpconrad@posteo.de>
2023-07-25 17:05:12 +0200

    create dir for json report

--- src/main/groovy/CucumberRunner.groovy
+++ src/main/groovy/CucumberRunner.groovy
@@ -1,9 +1,13 @@
 import io.cucumber.core.cli.Main
 
+import java.nio.file.Files
 import java.nio.file.Paths
 
 class CucumberRunner {
     static void main(String[] args) {
+        // Create the target directory if it doesn't exist
+        Files.createDirectories(Paths.get("target/cucumber-reports"))
+
         def options = [
                 'src/test/resources', // Directory where feature files are located
 //                '--glue', 'my.cucumber.project', // Package where step definitions are located
